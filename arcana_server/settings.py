4f091e86da35969b4bd8c1beb33b1cea5f79e8c1
Omkommersind <chauzov@list.ru>
2019-12-01 17:31:10 +0300

    Static settings

--- arcana_server/settings.py
+++ arcana_server/settings.py
@@ -140,6 +140,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'files', 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'files', 'media')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 DEFAULT_LANGUAGE_CODE = 'en'
